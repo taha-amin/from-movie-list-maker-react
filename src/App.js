50577d57ccac6aa53599aedbba00dc47089bff6a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-05 12:29:56 -0700

    tracking state for all necessary items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+//track state for allMovies, filteredMovies, movieFormYearReleased, movieFormDirector, movieTitle, movieFormColor
+//pass state as props correctly to MovieForm, Movie, and MovieList
+//define a handleDeleteMovie function that deleted a movie from the state array using
+//define a handleFilterMovies function that takes in a string and set filteredMovies to an array of movies whose name matches that string
+
 import './App.css';
 import { useState, useEffect } from 'react';
 import MovieForm from './MovieForm';
@@ -5,24 +10,13 @@ import MovieList from './MovieList';
 import Movie from './Movie';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setAllFilteredMovies] = useState([]);
+  const [movieFormYearReleased, setMovieFormYearReleased] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('lightblue');
+  const [filterQuery, setFilterQuery] = useState('');
+  return <div className="App"></div>;
 }
 
 export default App;
