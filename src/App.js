868922d0782c6e0d7dc318f897cbc6e66bebdcdc
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-05 11:57:27 -0700

    in app.js imported state and in movieform.js added a movie to state on submit and on changes for each input, called the appropriate state handler props with the correct e.target.value

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import MovieForm from './MovieForm';
+import MovieList from './MovieList';
+import Movie from './Movie';
 
 function App() {
   return (
